ba05bc625295e2b9ab51747aaeb4d163739222f4
neil-hughes <neil.hughes@techreturners.com>
2023-05-22 16:59:40 +0100

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Refactoring Exercise
+
+## 👋 Introduction
+
+This code produces sales information about transactions. Given an input set of data such as this:
+
+```
+['notebook', 'notebook', 'mouse', 'keyboard', 'mouse']
+```
+
+It produces a summary of what items have been purchased and how many of each item.
+
+The results are sorted by the number of transactions, then alphabetically.
+
+```
+['mouse 2', 'notebook 2', 'keyboard 1'].
+```
+
+Unfortunately, the code is not in an ideal state...
+
+---
+
+### 🗒️ Instructions
+
+✅ Please work through and refactor the provided code.
+
+✅ Keep notes on what you change and why.
+
+✅ We'd like you to talk through your changes with us for between 5 and 10 minutes. We're interested in how you present your ideas, what you improved, how you improved it, and what makes each change an improvement.
+
+💡 You can present your changes however you prefer - screen share, presentation, live code refactoring from old to new.
+
+✅ Note that all of the unit tests are currently passing, and they should continue to pass after you've made your changes.
+
+---
---
+++ src/transactions/transaction_processor.test.js
@@ -0,0 +1,54 @@
+const processTransactions = require('./transaction_processor');
+
+it('processes an empty array of transactions', () => {
+	expect(processTransactions([])).toEqual([]);
+});
+
+it('throws an exception if array of transactions is null', () => {
+	expect(() => processTransactions(null)).toThrow();
+});
+
+it('throws an exception if array of transactions is undefined', () => {
+	expect(() => processTransactions(undefined)).toThrow();
+});
+
+it('processes a single transaction', () => {
+	expect(processTransactions(['jumper'])).toEqual(['jumper 1']);
+});
+
+it('processes multiple transactions of the same item', () => {
+	expect(processTransactions(['jumper', 'jumper'])).toEqual(['jumper 2']);
+});
+
+it('processes multiple transactions of multiple items', () => {
+	expect(processTransactions(['jumper', 'jeans', 'jeans'])).toEqual([
+		'jeans 2',
+		'jumper 1',
+	]);
+});
+
+test('alphabetically orders multiple transactions', () => {
+	expect(processTransactions(['jumper', 'jeans'])).toEqual([
+		'jeans 1',
+		'jumper 1',
+	]);
+});
+
+test('numerically orders multiple transactions', () => {
+	expect(processTransactions(['hoody', 'jeans', 'jeans'])).toEqual([
+		'jeans 2',
+		'hoody 1',
+	]);
+});
+
+it('processes a group of transactions correctly', () => {
+	expect(
+		processTransactions([
+			'notebook',
+			'notebook',
+			'mouse',
+			'keyboard',
+			'mouse',
+		])
+	).toEqual(['mouse 2', 'notebook 2', 'keyboard 1']);
+});
